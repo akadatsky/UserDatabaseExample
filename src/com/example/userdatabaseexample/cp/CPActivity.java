d9be42ca72044b7339bdef38b2998c8d38ee06fd
andreykadatsky <andrey.kadatsky@mobindusry.net>
2014-02-04 15:36:38 +0200

    Using actual for Android 4.4 SimpleCursorAdapter, CursorLoader and ContentProvider

--- src/com/example/userdatabaseexample/cp/CPActivity.java
+++ src/com/example/userdatabaseexample/cp/CPActivity.java
@@ -1,126 +1,92 @@
 package com.example.userdatabaseexample.cp;
 
-import android.app.Activity;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 import com.example.userdatabaseexample.R;
-import com.example.userdatabaseexample.util.Lists;
 
-import java.util.List;
+public class CPActivity extends FragmentActivity {
 
-public class CPActivity extends Activity {
-
-  final String LOG_TAG = "myLogs";
-
-  final Uri CONTACT_URI = Uri.parse("content://ru.startandroid.providers.AdressBook/contacts");
+  public final static Uri CONTACT_URI = Uri.parse("content://ru.startandroid.providers.AdressBook/contacts");
+  public final static int CONTACT_LOADER_ID = 1;
 
   final String CONTACT_NAME = "name";
   final String CONTACT_EMAIL = "email";
+
+  private SimpleCursorAdapter adapter;
+
+  private LoaderManager.LoaderCallbacks<Cursor> loaderCallbacks = new LoaderManager.LoaderCallbacks<Cursor>() {
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+      if (id == CONTACT_LOADER_ID) {
+        return new CursorLoader(getApplicationContext(), CONTACT_URI, null, null, null, null);
+      }
+      return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+      if (loader.getId() == CONTACT_LOADER_ID) {
+        adapter.swapCursor(data);
+      }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+
+  };
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.cp);
 
-    Cursor cursor = getContentResolver().query(CONTACT_URI, null, null,
-        null, null);
-    startManagingCursor(cursor);
+    getSupportLoaderManager().initLoader(CONTACT_LOADER_ID, null, loaderCallbacks); // should be called in onCreate()
 
     String from[] = {"name", "email"};
     int to[] = {android.R.id.text1, android.R.id.text2};
-
-    SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor, from, to);
+    adapter = new SimpleCursorAdapter(
+        this, android.R.layout.simple_list_item_2, null, from, to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
 
     ListView lvContact = (ListView) findViewById(R.id.lvContact);
     lvContact.setAdapter(adapter);
+
+    getSupportLoaderManager().getLoader(CONTACT_LOADER_ID).startLoading();
   }
 
   public void onClickInsert(View v) {
     ContentValues cv = new ContentValues();
-    cv.put(CONTACT_NAME, "name 4");
-    cv.put(CONTACT_EMAIL, "email 4");
+    cv.put(CONTACT_NAME, "name");
+    cv.put(CONTACT_EMAIL, "email");
     Uri newUri = getContentResolver().insert(CONTACT_URI, cv);
-    Log.d(LOG_TAG, "insert, result Uri : " + newUri.toString());
-  }
 
-  public void onClickUpdate(View v) {
-    ContentValues cv = new ContentValues();
-    cv.put(CONTACT_NAME, "name 5");
-    cv.put(CONTACT_EMAIL, "email 5");
-    Uri uri = ContentUris.withAppendedId(CONTACT_URI, 2);
-    int cnt = getContentResolver().update(uri, cv, null, null);
-    Log.d(LOG_TAG, "update, count = " + cnt);
+    long id = ContentUris.parseId(newUri);
+    cv.put(CONTACT_NAME, "name " + id);
+    cv.put(CONTACT_EMAIL, "email " + id);
+    getContentResolver().update(newUri, cv, null, null);
   }
 
   public void onClickDelete(View v) {
-    Uri uri = ContentUris.withAppendedId(CONTACT_URI, 3);
-    int cnt = getContentResolver().delete(uri, null, null);
-    Log.d(LOG_TAG, "delete, count = " + cnt);
-  }
-
-  public void onClickError(View v) {
-    Uri uri = Uri.parse("content://ru.startandroid.providers.AdressBook/phones");
-    try {
-      Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-    } catch (Exception ex) {
-      Log.d(LOG_TAG, "Error: " + ex.getClass() + ", " + ex.getMessage());
+    Cursor cursor = getContentResolver().query(CONTACT_URI, null, null, null, null);
+    if ((cursor == null) || (cursor.getCount() == 0)) {
+      return;
     }
-
-  }
-
-  public void btnGetAllClick(View v) {
-    List<User> users = Lists.newArrayList();
-    String columns[] = {"_id", "name", "email"};
-    Cursor cursor = getContentResolver().query(CONTACT_URI, columns, null, null, "_id ASC");
-    cursor.moveToFirst();
-    while (!cursor.isAfterLast()) {
-      String name = cursor.getString(cursor.getColumnIndex("name"));
-      String email = cursor.getString(cursor.getColumnIndex("email"));
-      long id = cursor.getLong(cursor.getColumnIndex("_id"));
-      users.add(new User(name, email, id));
-      cursor.moveToNext();
-    }
-    Log.i(LOG_TAG, users.toString());
-  }
-
-  private class User {
-    private String name;
-    private String email;
-    private long id;
-
-    private User(String name, String email, long id) {
-      this.name = name;
-      this.email = email;
-      this.id = id;
-    }
-
-    public String getName() {
-      return name;
-    }
-
-    public String getEmail() {
-      return email;
-    }
-
-    public long getId() {
-      return id;
-    }
-
-    @Override
-    public String toString() {
-      return "User{" +
-          "id=" + id +
-          ", email='" + email + '\'' +
-          ", name='" + name + '\'' +
-          '}';
-    }
+    cursor.moveToLast();
+    long lastId = cursor.getLong(cursor.getColumnIndex("_id"));
+    Uri uri = ContentUris.withAppendedId(CONTACT_URI, lastId);
+    getContentResolver().delete(uri, null, null);
   }
 
 }
